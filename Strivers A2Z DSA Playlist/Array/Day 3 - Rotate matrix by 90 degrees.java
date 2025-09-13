902dbfa319068139ee8d6cbf5af9ca6eedfb4f95
Khushang Singh <147972452+KhushangSingh@users.noreply.github.com>
2025-09-13 23:52:59 +0530

    Create Day 3 - Rotate matrix by 90 degrees.java

---
+++ Strivers A2Z DSA Playlist/Array/Day 3 - Rotate matrix by 90 degrees.java
@@ -0,0 +1,49 @@
+// Rotate matrix by 90 degrees
+
+// 1. Brute Force Approach
+
+class Solution {
+    public void rotateMatrix(int[][] matrix) {
+
+        int n = matrix.length;
+        int[][] rotatedMatrix = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rotatedMatrix[j][n - 1 - i] = matrix[i][j];
+            }
+        }
+    }
+}
+
+// 2. Optimal Solution
+
+class Solution {
+    public void rotateMatrix(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            reverse(matrix[i]);
+        }
+    }
+
+    public static void reverse(int[] row) {
+        int start = 0;
+        int end = row.length - 1;
+        while (start < end) {
+            int temp = row[start];
+            row[start] = row[end];
+            row[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
